d24cf6296049012470ce7fd8a2db3e35db4f1127
Brandon Forys <bforys@gmail.com>
2020-06-27 16:38:12 -0700

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="deepcut2realtime", # Replace with your own username
+    name="deepcut2realtime",
     version="0.0.1",
     author="Brandon Forys",
     author_email="brandon.forys@alumni.ubc.ca",
